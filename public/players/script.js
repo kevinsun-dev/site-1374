2b1030ca09fa670589d6c0f1ff38c61c51e4857a
Kevin Sun <contact@kevinsun.dev>
2021-10-22 20:49:18 -0400

    Updates and fixes

--- public/players/script.js
+++ public/players/script.js
@@ -44,6 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
     playersRef.get().then((playersDoc) => {
       let index = 0;
       let chatList = [];
+      let solvedCount = {};
       playersDoc.forEach((playerDoc) => {
         let playerData = playerDoc.data();
         if (playerDoc.id > 0) {
@@ -62,11 +63,30 @@ document.addEventListener('DOMContentLoaded', function () {
           if (playerData.messages) {
             chatList = chatList.concat(playerData.messages);
           }
+
+          let board = [];
+          if (playerData['bingoBoard']) {
+            board = playerData['bingoBoard'];
+          }
+          board.forEach((it, i) => {
+            if (it.solved) {
+              if (solvedCount[it.puzzleId]) {
+                solvedCount[it.puzzleId]++;
+              } else {
+                solvedCount[it.puzzleId] = 1;
+              }
+            }
+          });
+
           playerContainer.appendChild(playerFace);
           colItem.appendChild(playerContainer);
         }
         index++;
       });
+
+      // Puzzles Solved
+      console.log(solvedCount);
+
       let sortedChat = chatList.sort((a, b) => b.date - a.date);
       sortedChat.forEach((msg) => {
         const chatMsg = document.createElement("p");
@@ -76,24 +96,26 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     sendMsg.addEventListener("click", () => {
-      const playerDocRef = db.collection('players').doc(savedPlayerId);
-      playerDocRef.get().then((playerDoc) => {
-        let playerData = playerDoc.data();
-        let msgList = [];
-        if (playerData['messages']) {
-          msgList = playerData['messages'];
-        } else {
-          playerData['messages'] = msgList;
-        }
-        msgList.push({ title: `Player ${savedPlayerId}: ${inputMsg.value}`, date: new Date() });
+      if (inputMsg.value.length > 0) {
+        const playerDocRef = db.collection('players').doc(savedPlayerId);
+        playerDocRef.get().then((playerDoc) => {
+          let playerData = playerDoc.data();
+          let msgList = [];
+          if (playerData['messages']) {
+            msgList = playerData['messages'];
+          } else {
+            playerData['messages'] = msgList;
+          }
+          msgList.push({ title: `Player ${savedPlayerId}: ${inputMsg.value}`, date: new Date() });
 
-        playerDocRef.set(playerData).then(() => {
-          const chatMsg = document.createElement("p");
-          chatMsg.innerText = `Player ${savedPlayerId}: ${inputMsg.value}`;
-          chatContainerDisp.prepend(chatMsg);
-          inputMsg.value = "";
+          playerDocRef.set(playerData).then(() => {
+            const chatMsg = document.createElement("p");
+            chatMsg.innerText = `Player ${savedPlayerId}: ${inputMsg.value}`;
+            chatContainerDisp.prepend(chatMsg);
+            inputMsg.value = "";
+          });
         });
-      });
+      }
     });
 
     inputMsg.addEventListener("keyup", function (event) {
